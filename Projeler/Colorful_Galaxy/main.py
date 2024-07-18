e53495f846e297f3cbe7e38c69cc04fb2679fa6e
keremuzunboy <kerem.uzunby@gmail.com>
2024-07-18 18:13:48 +0300

    feat: Add colorful galaxy drawing feature

---
+++ Projeler/Colorful_Galaxy/main.py
@@ -0,0 +1,13 @@
+import turtle
+t = turtle.Turtle()
+s = turtle.Screen()
+colors = ['orange', 'red', 'magenta', 'blue', 'magenta', 'blue', 'magenta', 'yellow', 'green', 'cyan', 'purple']
+s.bgcolor('black')
+t.pensize(2)
+t.speed(0)
+for x in range(360):
+    t.pencolor(colors[x % 6])
+    t.width(x // 100 + 1)
+    t.forward(x)
+    t.right(59)
+    turtle.hideturtle()
